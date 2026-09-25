10d24b7700cc6b33e5cc049e25fd939ad0818e04
nk9571718-cpu <nk9571718@gmail.com>
2026-09-25 18:56:59 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,7 +1,8 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 @app.route("/")
 def home():
   return "<h1>Welcome to Rendor</h1><p>cloud computing test</p>"
   if__name__ == "__main__":
-  app.run()
+  app.run(host="0.0.0.0", port int(os.environ.get("PORT",10000)))
