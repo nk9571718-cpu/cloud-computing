038ef2cea2dc8863f18ecd9b96e62734b21498df
nk9571718-cpu <nk9571718@gmail.com>
2026-09-25 18:39:00 +0530

    app1.py

---
+++ app1.py
@@ -0,0 +1,7 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def home():
+  return "<h1>Welcome to Rendor</h1><p>cloud computing test</p>"
+  if__name__ == "__main__":
+  app.run()
